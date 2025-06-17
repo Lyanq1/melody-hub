333d4b309407a1bf2196ef222c9f9c065dd22c78
Quan Huynh <163743120+hmquan191@users.noreply.github.com>
2025-06-17 21:16:05 +0700

    update thu len vercel

--- BackEnd/controllers/disc.controller.js
+++ BackEnd/controllers/disc.controller.js
@@ -1,14 +1,15 @@
 import Disc from '../models/disc.model.js'
+// nháp comment
 
 export const getAllDiscs = async (req, res) => {
   try {
-    const discs = await Disc.find({});
-    console.log(discs);
+    const discs = await Disc.find({})
+    console.log(discs)
     if (discs.length === 0) {
-      return res.status(404).json({ message: 'No discs found' });
+      return res.status(404).json({ message: 'No discs found' })
     }
-    res.json(discs);
+    res.json(discs)
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching discs', error: error.message });
+    res.status(500).json({ message: 'Error fetching discs', error: error.message })
   }
-};
+}
