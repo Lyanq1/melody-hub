90a9293921fa4268c1964f68d17e00924facb182
vi-del <trantuongvi2908@gmail.com>
2025-06-22 11:57:21 +0700

    Add footer

--- FrontEnd/app/layout.tsx
+++ FrontEnd/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Navbar } from '@/components/ui/navbar'
-
+import Footer from '@/components/Footer'
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin']
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-full flex-col`}>
         <Navbar />
         <main className='container mx-auto flex-grow'>{children}</main>
+        <Footer />
       </body>
     </html>
   )
