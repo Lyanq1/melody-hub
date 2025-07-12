fa9900aeeb7c889b6646c81e31a094f0ac22d5d7
Quan Huynh <163743120+hmquan191@users.noreply.github.com>
2025-07-12 15:22:51 +0700

    cập nhật lại số lượng sản phẩm hiện trong cart của navigation bar

--- FrontEnd/components/ui/navbar.tsx
+++ FrontEnd/components/ui/navbar.tsx
@@ -112,7 +112,6 @@ import Link from 'next/link'
 import './components.css'
 import { useState, useEffect } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import Image from 'next/image'
 
 export const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -121,8 +120,8 @@ export const Navbar = () => {
   useEffect(() => {
     const getCartCount = () => {
       const cart = JSON.parse(localStorage.getItem('cart') || '[]')
-      const total = cart.reduce((sum: number, item: { quantity: number }) => sum + item.quantity, 0)
-      setCartCount(total)
+      const uniqueCount = cart.length // mỗi item có id khác nhau
+      setCartCount(uniqueCount)
     }
 
     getCartCount()
@@ -179,10 +178,21 @@ export const Navbar = () => {
         </div>
         <div className='flex gap-5 text-center'>
           <Link href='/cart' className='relative flex items-center gap-2'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-shopping-cart-icon lucide-shopping-cart cursor-pointer">
-              <circle cx="8" cy="21" r="1"/>
-              <circle cx="19" cy="21" r="1"/>
-              <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='28'
+              height='28'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='#ffffff'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-shopping-cart-icon lucide-shopping-cart cursor-pointer'
+            >
+              <circle cx='8' cy='21' r='1' />
+              <circle cx='19' cy='21' r='1' />
+              <path d='M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12' />
             </svg>
             {cartCount >= 1 && (
               <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center'>
@@ -194,9 +204,20 @@ export const Navbar = () => {
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <button className='flex items-center gap-2 focus:outline-none'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round-icon lucide-user-round cursor-pointer">
-                  <circle cx="12" cy="8" r="5"/>
-                  <path d="M20 21a8 8 0 0 0-16 0"/>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='28'
+                  height='28'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='#ffffff'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='lucide lucide-user-round-icon lucide-user-round cursor-pointer'
+                >
+                  <circle cx='12' cy='8' r='5' />
+                  <path d='M20 21a8 8 0 0 0-16 0' />
                 </svg>
               </button>
             </DropdownMenuTrigger>
@@ -216,18 +237,18 @@ export const Navbar = () => {
                   className='group flex items-center gap-2 px-4 py-2 hover:bg-gray-700 rounded-md transition-colors duration-200'
                 >
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#ffffff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-heart-icon lucide-heart transition-colors duration-200 group-hover:stroke-black"
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='#ffffff'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='lucide lucide-heart-icon lucide-heart transition-colors duration-200 group-hover:stroke-black'
                   >
-                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/>
+                    <path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
                   </svg>
                   Wishlist
                 </Link>
@@ -238,18 +259,24 @@ export const Navbar = () => {
                   className='group flex items-center gap-2 px-4 py-2 hover:bg-gray-700 rounded-md transition-colors duration-200'
                 >
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#ffffff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-shopping-basket-icon lucide-shopping-basket transition-colors duration-200 group-hover:stroke-black"
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='#ffffff'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='lucide lucide-shopping-basket-icon lucide-shopping-basket transition-colors duration-200 group-hover:stroke-black'
                   >
-                    <path d="m15 11-1 9"/><path d="m19 11-4-7"/><path d="M2 11h20"/><path d="m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4"/><path d="M4.5 15.5h15"/><path d="m5 11 4-7"/><path d="m9 11 1 9"/>
+                    <path d='m15 11-1 9' />
+                    <path d='m19 11-4-7' />
+                    <path d='M2 11h20' />
+                    <path d='m3.5 11 1.6 7.4a2 2 0 0 0 2 1.6h9.8a2 2 0 0 0 2-1.6l1.7-7.4' />
+                    <path d='M4.5 15.5h15' />
+                    <path d='m5 11 4-7' />
+                    <path d='m9 11 1 9' />
                   </svg>
                   Theo dõi đơn hàng
                 </Link>
@@ -262,20 +289,20 @@ export const Navbar = () => {
                       className='group flex items-center gap-2 px-4 py-2 hover:bg-gray-700 rounded-md transition-colors duration-200'
                     >
                       <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="#ffffff"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-circle-user-round-icon lucide-circle-user-round transition-colors duration-200 group-hover:stroke-black"
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='#ffffff'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='lucide lucide-circle-user-round-icon lucide-circle-user-round transition-colors duration-200 group-hover:stroke-black'
                       >
-                        <path d="M18 20a6 6 0 0 0-12 0"/>
-                        <circle cx="12" cy="10" r="4"/>
-                        <circle cx="12" cy="12" r="10"/>
+                        <path d='M18 20a6 6 0 0 0-12 0' />
+                        <circle cx='12' cy='10' r='4' />
+                        <circle cx='12' cy='12' r='10' />
                       </svg>
                       Register
                     </Link>
@@ -286,20 +313,20 @@ export const Navbar = () => {
                       className='group flex items-center gap-2 px-4 py-2 hover:bg-gray-700 rounded-md transition-colors duration-200'
                     >
                       <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="#ffffff"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-key-icon lucide-key transition-colors duration-200 group-hover:stroke-black"
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='#ffffff'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='lucide lucide-key-icon lucide-key transition-colors duration-200 group-hover:stroke-black'
                       >
-                        <path d="m15.5 7.5 2.3 2.3a1 1 0 0 0 1.4 0l2.1-2.1a1 1 0 0 0 0-1.4L19 4"/>
-                        <path d="m21 2-9.6 9.6"/>
-                        <circle cx="7.5" cy="15.5" r="5.5"/>
+                        <path d='m15.5 7.5 2.3 2.3a1 1 0 0 0 1.4 0l2.1-2.1a1 1 0 0 0 0-1.4L19 4' />
+                        <path d='m21 2-9.6 9.6' />
+                        <circle cx='7.5' cy='15.5' r='5.5' />
                       </svg>
                       Login
                     </Link>
